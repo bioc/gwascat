8aca63d53e8519c63947a9d944372966b504774a
Vince Carey <stvjc@channing.harvard.edu>
2020-10-14 22:57:09 -0400

    remove dependence on gQTLstats

--- R/ldtagr.R
+++ R/ldtagr.R
@@ -1,11 +1,9 @@
-#' expand a list of variants by including those in a VCF with LD exceeding some
-#' threshold
 #' 
 #' expand a list of variants by including those in a VCF with LD exceeding some
-#' threshold
-#' 
-#' uses snpStats ld()
-#' 
+#' threshold; uses snpStats ld()
+#' @import snpStats
+#' @import VariantAnnotation
+#' @import S4Vectors
 #' @param snprng a named GRanges for a single SNP.  The name must correspond to
 #' the name that will be assigned by genotypeToSnpMatrix (from VariantTools) to
 #' the corresponding column of a SnpMatrix.
@@ -22,19 +20,12 @@
 #' @author VJ Carey
 #' @keywords models
 #' @examples
-#' 
-#' require(GenomicRanges)
-#' if (requireNamespace("gQTLstats")) {
-#'    # install gQTLstats to test this function
-#'  cand = GRanges("1", IRanges(113038694, width=1))
+#'  cand = GenomicRanges::GRanges("1", IRanges::IRanges(113038694, width=1))
 #'  names(cand) = "rs883593"
-#'  require(VariantAnnotation)
+#'  requireNamespace("VariantAnnotation")
 #'  expath = dir(system.file("vcf", package="gwascat"), patt=".*exon.*gz$", full=TRUE)
-#'  tf = TabixFile(expath)
+#'  tf = Rsamtools::TabixFile(expath)
 #'  ldtagr( cand, tf, lbR2 = .8)
-#' }
-#' # should do with 1000 genomes in S3 bucket and gwascat
-#' 
 #' @export ldtagr
 ldtagr = function( snprng, tf, samples, genome="hg19",
    lbmaf=.05, lbR2=.8, radius=100000 ) {
@@ -46,13 +37,12 @@ ldtagr = function( snprng, tf, samples, genome="hg19",
   stopifnot(length(snprng)==1)
   snpid = names(snprng)
   stopifnot(length(snpid)==1)
-  if (!requireNamespace("gQTLstats")) stop("install gQTLstats to use this function")
   if (!requireNamespace("DelayedArray")) stop("install DelayedArray to use this function")
   if (!requireNamespace("snpStats")) stop("install snpStats to use this function")
-  quer = gQTLstats::queryVCF( gr=snprng+radius, vcf.tf=tf, 
+  quer = queryVCF( gr=snprng+radius, vcf.tf=tf, 
          samps=samples, genome=genome, getSM=TRUE )
   empty = GRanges()
-  mcols(empty) = DataFrame(paramRangeID = factor(), R2 = numeric())
+  mcols(empty) = S4Vectors::DataFrame(paramRangeID = factor(), R2 = numeric())
   vcfrng = DelayedArray::rowRanges(quer$readout)
   gt = quer$sm$genotypes
   if (!(snpid %in% colnames(gt))) {
@@ -77,3 +67,27 @@ ldtagr = function( snprng, tf, samples, genome="hg19",
   names(extrng) = make.names(thec)
   extrng[, c("paramRangeID", "R2")]
 }
+
+queryVCF = function (gr, vcf.tf, samps, genome = "hg19", getSM=TRUE, snvOnly=TRUE)
+{
+#
+# support anonymous extraction of VCF/SnpMatrix by range
+#
+    vp = ScanVcfParam(fixed = "ALT", info = NA, geno = "GT",
+        which = gr)
+    if (!missing(samps)) {
+      present = samples(scanVcfHeader(vcf.tf))
+      if((dl <- length(setdiff(samps, present))) > 0) {
+        message(paste0("NOTE: there were ", dl, " samples not found (of ",
+            length(samps)," requested)."))
+        }
+      vcfSamples(vp) = (int <- intersect(samps, present))
+      stopifnot(length(int)>0)
+      }
+    readout = readVcf(vcf.tf, genome = genome, param = vp)
+    if (snvOnly) readout = readout[which(isSNV(readout)),]
+    sm = NULL
+    if (getSM) sm = genotypeToSnpMatrix(readout)
+    list(readout=readout, sm=sm)
+}
+
